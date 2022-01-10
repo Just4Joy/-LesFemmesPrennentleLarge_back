b97d2f2e8cd2bcbeae164b140368f1f7ecd50c44
ChleaBourtoule <chlea.bourtoule@gmail.com>
2022-01-10 12:02:32 +0100

    user findMany modified

--- models/user.ts
+++ models/user.ts
@@ -61,7 +61,7 @@ const validateLogin = (req: Request, res: Response, next: NextFunction) => {
 
 const findMany = () => {
   const sql =
-    'SELECT u.city, u.created_date, u.email, u.favorite_spot, u.firstname, d.department_name, sk.name, st.name_user, u.id_user, u.lastname, u.password, u.profile_pic, u.wahine, u.zip_code FROM users u INNER JOIN departments d ON u.id_departement = d.id_department INNER JOIN surf_skills sk ON u.id_surf_skill = sk.id_surf_skill INNER JOIN surf_styles st ON u.id_surf_style = st.id_surf_style';
+    'SELECT u.city, u.created_date, u.email, u.favorite_spot, u.firstname, d.department_name as department, sk.name as surf_skill, st.name_user as surf_style, u.id_user, u.lastname, u.password, u.profile_pic, u.wahine, u.zip_code FROM users u INNER JOIN departments d ON u.id_departement = d.id_department INNER JOIN surf_skills sk ON u.id_surf_skill = sk.id_surf_skill INNER JOIN surf_styles st ON u.id_surf_style = st.id_surf_style';
   return connection
     .promise()
     .query<IUser[]>(sql, [])
